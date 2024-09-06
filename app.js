ce8114f89ccef92a1692934e2be5f9284a416713
Mateus Barros <mateusverissimo94@gmail.com>
2024-09-06 15:35:49 -0300

    finalizando

--- app.js
+++ app.js
@@ -1,43 +1,66 @@
 function pesquisar() {
-  const nomeAtleta = document.querySelector("input").value;
   let section = document.getElementById("resultados-pesquisa");
+  let nomePokemon = campoPesquisa.value;
+
+  if (!nomePokemon) {
+    section.innerHTML =
+      "<p>Nada foi encontrada. Você precisa digitar o nome de um Pokémon ou pelo seu número (1 a 151)</p>";
+    return;
+  }
+
+  nomePokemon = nomePokemon.toLowerCase();
 
   let resultados = "";
+  let nome = "";
+  let descricao = "";
+  let tags = "";
 
   for (let dado of dados) {
-    nomeSeparado = dado.titulo.toLowerCase().split(" ");
-    console.log(nomeSeparado);
+    nome = dado.nome.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    tags = dado.tags.toLowerCase();
 
-    if (nomeSeparado.includes(nomeAtleta)) {
+    if (
+      nome.includes(nomePokemon) ||
+      descricao.includes(nomePokemon) ||
+      tags.includes(nomePokemon)
+    ) {
       resultados += `
         <div class="item-resultado">
+          <div class="item-resultado-imagem">
+            <img
+              src="${dado.image}"
+              alt="Gif de um ${dado.nome}"
+            />
+          </div>
+          <div class="item-resultado-dados">
             <h2>
-                <a href="#" target="_blank">${dado.titulo}</a>
+              <a href="#" target="_blank">${dado.id} - ${dado.nome}</a>
             </h2>
-            <p class="descricao-meta">${dado.descricao}</p>
-            <a href=${dado.link} target="_blank">Mais informações</a>
+            <p class="descricao-meta">
+              ${dado.descricao}
+            </p>
+            <a
+              href="${dado.link}"
+              target="_blank"
+              >Mais informações</a
+            >
+          </div>
         </div>
         `;
-      break;
     }
   }
 
-  if (resultados === "") {
-    resultados += `
-    <div class="item-resultado">
-        <h2>
-            Atleta não encontrada, Digite outro nome!
-        </h2>
-    </div>
-    `;
+  if (!resultados) {
+    resultados += "Nada foi encontrado!";
   }
 
   section.innerHTML = resultados;
 }
 
-const input = document.getElementById("input");
+const campoPesquisa = document.getElementById("campo-pesquisa");
 
-input.addEventListener("keyup", function (event) {
+campoPesquisa.addEventListener("keyup", (event) => {
   if (event.key === "Enter") {
     pesquisar();
   }
--- dados.js
+++ dados.js
@@ -1,20 +1,92 @@
 let dados = [
   {
-    titulo: "Rayssa Leal",
+    id: "001",
+    nome: "Bulbasaur",
     descricao:
-      "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
-    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+      "Um Pokémon tipo Planta/Venenoso. Ele armazena energia solar no bulbo em suas costas e libera em ataques poderosos.",
+    link: "https://www.pokemon.com/br/pokedex/bulbasaur",
+    tags: "planta venenoso bulbasauro ash pokemon pokémon",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/1.gif",
   },
   {
-    titulo: "Beatriz Souza",
+    id: "002",
+    nome: "Ivysaur",
     descricao:
-      "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro e inspirando novas gerações de atletas.",
-    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+      "A evolução de Bulbasaur. Quando o bulbo em suas costas floresce, ele fica mais forte e pronto para evoluir.",
+    link: "https://www.pokemon.com/br/pokedex/Ivysaur",
+    tags: "planta venenoso ivisauro pokemon pokémon",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/2.gif",
   },
   {
-    titulo: "Rebeca Andrade",
+    id: "003",
+    nome: "Venusaur",
     descricao:
-      "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
-    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+      "A evolução final de Bulbasaur. A flor em suas costas libera uma fragrância que acalma os que estão por perto.",
+    link: "https://www.pokemon.com/br/pokedex/Venusaur",
+    tags: "planta venenoso venosauro pokemon pokémon",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/3.gif",
+  },
+  {
+    id: "004",
+    nome: "Charmander",
+    descricao:
+      "Um Pokémon do tipo Fogo. Sua cauda flamejante reflete seu humor e vitalidade. Se a chama apagar, ele morre.",
+    link: "https://www.pokemon.com/br/pokedex/Charmander",
+    tags: "fogo ash pokemon pokémon",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/4.gif",
+  },
+  {
+    id: "005",
+    nome: "Charmeleon",
+    descricao:
+      "A evolução de Charmander. Ele usa sua cauda flamejante para atacar seus oponentes e é conhecido por seu temperamento feroz.",
+    link: "https://www.pokemon.com/br/pokedex/Charmeleon",
+    tags: "fogo ash pokemon pokémon",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/5.gif",
+  },
+  {
+    id: "006",
+    nome: "Charizard",
+    descricao:
+      "A evolução final de Charmander. Charizard é capaz de cuspir fogo tão quente que pode derreter pedras. Seu voo poderoso o torna temível.",
+    link: "https://www.pokemon.com/br/pokedex/Charizard",
+    tags: "fogo voador ash pokemon pokémon",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/6.gif",
+  },
+  {
+    id: "007",
+    nome: "Squirtle",
+    descricao:
+      "Um Pokémon do tipo Água. Usa sua concha para proteção e esguicha água para atacar os inimigos.",
+    link: "https://www.pokemon.com/br/pokedex/Squirtle",
+    tags: "água tartaruga ash pokemon pokémon",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/7.gif",
+  },
+  {
+    id: "008",
+    nome: "Wartortle",
+    descricao:
+      "A evolução de Squirtle. Suas orelhas longas e cauda espessa ajudam a manter o equilíbrio na água e também o protegem.",
+    link: "https://www.pokemon.com/br/pokedex/Wartortle",
+    tags: "água tartaruga pokemon pokémon",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/8.gif",
+  },
+  {
+    id: "009",
+    nome: "Blastoise",
+    descricao:
+      "A forma final de Squirtle. Seus canhões de água disparam jatos potentes capazes de perfurar aço.",
+    link: "https://www.pokemon.com/br/pokedex/Blastoise",
+    tags: "água tartaruga pokemon pokémon",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/9.gif",
   },
 ];
