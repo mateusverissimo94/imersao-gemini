0473bf08c0e6d7e2c713212c7734066ac45b940e
Mateus Barros <mateusverissimo94@gmail.com>
2024-09-07 11:51:06 -0300

    projeto versão1.0 concluido e adicionando dados de pokemons e digimons

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
-function pesquisar() {
+function pesquisarPokemon() {
   let section = document.getElementById("resultados-pesquisa");
-  let nomePokemon = campoPesquisa.value;
+  let nomeDados = campoPesquisa.value;
 
-  if (!nomePokemon) {
-    section.innerHTML =
-      "<p>Nada foi encontrada. Você precisa digitar o nome de um Pokémon ou pelo seu número (1 a 151)</p>";
+  if (!nomeDados) {
+    section.innerHTML = `
+        <div class="item-resultado">
+          <div class="item-resultado-dados">
+            <h2>
+              Nada foi encontrada. Você precisa digitar o nome de um Pokémon ou pelo seu número (1 a 151)!
+            </h2>
+          </div>
+        </div>
+        `;
     return;
   }
 
-  nomePokemon = nomePokemon.toLowerCase();
+  nomeDados = nomeDados.toLowerCase();
 
   let resultados = "";
   let nome = "";
   let descricao = "";
   let tags = "";
+  let tipos = "";
 
-  for (let dado of dados) {
+  for (let dado of dadosPokemon) {
     nome = dado.nome.toLowerCase();
     descricao = dado.descricao.toLowerCase();
     tags = dado.tags.toLowerCase();
+    tipos = dado.tipos.join(", ").toLowerCase();
 
     if (
-      nome.includes(nomePokemon) ||
-      descricao.includes(nomePokemon) ||
-      tags.includes(nomePokemon)
+      nome.includes(nomeDados) ||
+      descricao.includes(nomeDados) ||
+      tags.includes(nomeDados) ||
+      tipos.includes(nomeDados)
     ) {
       resultados += `
         <div class="item-resultado">
@@ -35,7 +45,12 @@ function pesquisar() {
           </div>
           <div class="item-resultado-dados">
             <h2>
-              <a href="#" target="_blank">${dado.id} - ${dado.nome}</a>
+              <a href="${dado.link}" target="_blank">${dado.id} - ${
+        dado.nome
+      }</a>
+              <p class="paragrafo-tipos">Tipo: ${dado.tipos
+                .map((type) => `<i class="${type.toLowerCase()}">${type}</i>`)
+                .join("/")}</p>
             </h2>
             <p class="descricao-meta">
               ${dado.descricao}
@@ -52,10 +67,131 @@ function pesquisar() {
   }
 
   if (!resultados) {
-    resultados += "Nada foi encontrado!";
+    resultados += `
+        <div class="item-resultado">
+          <div class="item-resultado-dados">
+            <h2>
+              Nenhum Pokémon foi encontrado, Tente entre os primeiros 151!
+            </h2>
+          </div>
+        </div>
+        `;
   }
 
+  campoPesquisa.value = "";
   section.innerHTML = resultados;
+}
+
+function pesquisarDigimon() {
+  let section = document.getElementById("resultados-pesquisa");
+  let nomeDados = campoPesquisa.value;
+
+  if (!nomeDados) {
+    section.innerHTML = `
+        <div class="item-resultado">
+          <div class="item-resultado-dados">
+            <h2>
+              Nenhum Digimon foi encontrado, tente entre os parceiros dos Digiescolhidos!
+            </h2>
+          </div>
+        </div>
+        `;
+    return;
+  }
+
+  nomeDados = nomeDados.toLowerCase();
+
+  let resultados = "";
+  let nome = "";
+  let descricao = "";
+  let tags = "";
+  let nivel = "";
+  let atributo = "";
+
+  for (let dado of dadosDigimon) {
+    nome = dado.nome.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    tags = dado.tags.toLowerCase();
+    nivel = dado.nivel.toLowerCase();
+    atributo = dado.atributo.toLowerCase();
+
+    if (
+      nome.includes(nomeDados) ||
+      descricao.includes(nomeDados) ||
+      tags.includes(nomeDados) ||
+      nivel.includes(nomeDados) ||
+      atributo.includes(nomeDados)
+    ) {
+      resultados += `
+        <div class="item-resultado">
+          <div class="item-resultado-imagem">
+            <img
+              src="${dado.image}"
+              alt="Gif de um ${dado.nome}"
+            />
+          </div>
+          <div class="item-resultado-dados">
+            <div class="cabecalho">
+              <h2>
+                <a href="${dado.link}" target="_blank">${dado.nome}</a>
+              </h2>
+              <p>Nível: ${dado.nivel}</p>
+              <p>Atributo: ${dado.atributo}</p>
+            </div>
+            <p class="descricao-meta">
+              ${dado.descricao}
+            </p>
+            <a
+              href="${dado.link}"
+              target="_blank"
+              >Mais informações</a
+            >
+          </div>
+        </div>
+        `;
+    }
+  }
+
+  if (!resultados) {
+    resultados += `
+        <div class="item-resultado">
+          <div class="item-resultado-dados">
+            <h2>
+              Nenhum Digimon foi encontrado, tente entre os parceiros dos Digiescolhidos!
+            </h2>
+          </div>
+        </div>
+        `;
+  }
+
+  campoPesquisa.value = "";
+  section.innerHTML = resultados;
+}
+
+function mudarTema() {
+  const body = document.querySelector("body");
+  const titulo = document.querySelector("h1");
+  const input = document.querySelector("input");
+  const botaoPesquisa = document.querySelector(".botao-pesquisa");
+
+  const ativarScriptDigimon = body.classList.contains("tema-digimon");
+
+  body.classList.toggle("tema-digimon");
+  titulo.classList.toggle("tema-digimon");
+  input.classList.toggle("tema-digimon");
+
+  if (ativarScriptDigimon) {
+    botaoPesquisa.setAttribute("onClick", "pesquisarPokemon()");
+    botaoAlterarTema.textContent = "Digimon";
+    input.placeholder =
+      "Digite o número, nome, tipo ou no que o pokémon foi inspirado";
+    titulo.textContent = "Procure por seu Pokémon favorito";
+  } else {
+    botaoPesquisa.setAttribute("onClick", "pesquisarDigimon()");
+    botaoAlterarTema.textContent = "Pokémon";
+    input.placeholder = "Digite o nome dele ou o Digiescolhido ligado a ele";
+    titulo.textContent = "Procure por seu Digimon favorito";
+  }
 }
 
 const campoPesquisa = document.getElementById("campo-pesquisa");
@@ -64,4 +200,10 @@ campoPesquisa.addEventListener("keyup", (event) => {
   if (event.key === "Enter") {
     pesquisar();
   }
+});
+
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+
+botaoAlterarTema.addEventListener("click", () => {
+  mudarTema();
 });
--- assets/js/dados.js
+++ assets/js/dados.js
@@ -1,7 +1,8 @@
-let dados = [
+let dadosPokemon = [
   {
     id: "001",
     nome: "Bulbasaur",
+    tipos: ["Grama", "Venenoso"],
     descricao:
       "Um Pokémon tipo Planta/Venenoso. Ele armazena energia solar no bulbo em suas costas e libera em ataques poderosos.",
     link: "https://www.pokemon.com/br/pokedex/bulbasaur",
@@ -12,6 +13,7 @@ let dados = [
   {
     id: "002",
     nome: "Ivysaur",
+    tipos: ["Grama", "Venenoso"],
     descricao:
       "A evolução de Bulbasaur. Quando o bulbo em suas costas floresce, ele fica mais forte e pronto para evoluir.",
     link: "https://www.pokemon.com/br/pokedex/Ivysaur",
@@ -22,6 +24,7 @@ let dados = [
   {
     id: "003",
     nome: "Venusaur",
+    tipos: ["Grama", "Venenoso"],
     descricao:
       "A evolução final de Bulbasaur. A flor em suas costas libera uma fragrância que acalma os que estão por perto.",
     link: "https://www.pokemon.com/br/pokedex/Venusaur",
@@ -32,6 +35,7 @@ let dados = [
   {
     id: "004",
     nome: "Charmander",
+    tipos: ["Fogo"],
     descricao:
       "Um Pokémon do tipo Fogo. Sua cauda flamejante reflete seu humor e vitalidade. Se a chama apagar, ele morre.",
     link: "https://www.pokemon.com/br/pokedex/Charmander",
@@ -42,6 +46,7 @@ let dados = [
   {
     id: "005",
     nome: "Charmeleon",
+    tipos: ["Fogo"],
     descricao:
       "A evolução de Charmander. Ele usa sua cauda flamejante para atacar seus oponentes e é conhecido por seu temperamento feroz.",
     link: "https://www.pokemon.com/br/pokedex/Charmeleon",
@@ -52,6 +57,7 @@ let dados = [
   {
     id: "006",
     nome: "Charizard",
+    tipos: ["Fogo", "Voador"],
     descricao:
       "A evolução final de Charmander. Charizard é capaz de cuspir fogo tão quente que pode derreter pedras. Seu voo poderoso o torna temível.",
     link: "https://www.pokemon.com/br/pokedex/Charizard",
@@ -62,6 +68,7 @@ let dados = [
   {
     id: "007",
     nome: "Squirtle",
+    tipos: ["Agua"],
     descricao:
       "Um Pokémon do tipo Água. Usa sua concha para proteção e esguicha água para atacar os inimigos.",
     link: "https://www.pokemon.com/br/pokedex/Squirtle",
@@ -72,6 +79,7 @@ let dados = [
   {
     id: "008",
     nome: "Wartortle",
+    tipos: ["Agua"],
     descricao:
       "A evolução de Squirtle. Suas orelhas longas e cauda espessa ajudam a manter o equilíbrio na água e também o protegem.",
     link: "https://www.pokemon.com/br/pokedex/Wartortle",
@@ -82,11 +90,92 @@ let dados = [
   {
     id: "009",
     nome: "Blastoise",
+    tipos: ["Agua"],
     descricao:
       "A forma final de Squirtle. Seus canhões de água disparam jatos potentes capazes de perfurar aço.",
     link: "https://www.pokemon.com/br/pokedex/Blastoise",
     tags: "água tartaruga pokemon pokémon",
     image:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/9.gif",
+  },
+  {
+    id: "010",
+    nome: "Caterpie",
+    tipos: ["Inseto"],
+    descricao:
+      "Um Pokémon do tipo Inseto. Ele se move devagar, mas pode usar seda para se proteger de predadores.",
+    link: "https://www.pokemon.com/br/pokedex/Caterpie",
+    tags: "inseto larva ash pokemon pokémon",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/10.gif",
+  },
+  {
+    id: "011",
+    nome: "Metapod",
+    tipos: ["Inseto"],
+    descricao:
+      "A evolução de Caterpie. Sua casca rígida o protege enquanto se prepara para evoluir.",
+    link: "https://www.pokemon.com/br/pokedex/Metapod",
+    tags: "inseto casulo ash pokemon pokémon",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/11.gif",
+  },
+  {
+    id: "012",
+    nome: "Butterfree",
+    tipos: ["Inseto", "Voador"],
+    descricao:
+      "A evolução final de Caterpie. Suas asas liberam pólen venenoso, e ele é um excelente polinizador.",
+    link: "https://www.pokemon.com/br/pokedex/Butterfree",
+    tags: "inseto borbaleta ash pokemon pokémon",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/12.gif",
+  },
+  {
+    id: "013",
+    nome: "Weedle",
+    tipos: ["Inseto", "Venenoso"],
+    descricao:
+      "Um Pokémon tipo Inseto/Venenoso. Seu ferrão na cabeça é venenoso e ele usa para se defender.",
+    link: "https://www.pokemon.com/br/pokedex/Weedle",
+    tags: "inseto larva pokemon pokémon",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/13.gif",
+  },
+  {
+    id: "014",
+    nome: "Kakuna",
+    tipos: ["Inseto", "Venenoso"],
+    descricao:
+      "A evolução de Weedle. Embora pareça imóvel, está em processo de evolução interna.",
+    link: "https://www.pokemon.com/br/pokedex/Kakuna",
+    tags: "inseto borbaleta ash pokemon pokémon",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/14.gif",
+  },
+  {
+    id: "015",
+    nome: "Beedrill",
+    tipos: ["Inseto", "Venenoso"],
+    descricao:
+      "A evolução final de Weedle. Com três ferrões venenosos, Beedrill é incrivelmente territorial e agressivo.",
+    link: "https://www.pokemon.com/br/pokedex/Beedrill",
+    tags: "inseto borbaleta ash pokemon pokémon",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/15.gif",
+  },
+];
+
+let dadosDigimon = [
+  {
+    id: "001",
+    nome: "Agumon",
+    nivel: "Rookie",
+    atributo: "Vaccine",
+    descricao:
+      "Um Digimon do tipo Reptil, estágio Criança. É o parceiro de Tai (Taichi Yagami) e pode evoluir para formas poderosas. Agumon é corajoso, leal e possui um ataque de fogo chamado 'Chama Bebê'. Sua relação com Tai é baseada em confiança e liderança",
+    link: "https://digitalmonster.fandom.com/pt/wiki/Agumon",
+    tags: "dinossauro tai digiescolhido digimon rookie vaccine",
+    image: "https://digimon.shadowsmith.com/img/agumon.jpg",
   },
 ];
